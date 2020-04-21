37fb0693ced9d6117eaa32bd25034af348afc9eb
Tanesh Gupta <taneshgupta2211@gmail.com>
2020-04-21 18:40:57 +0530

    Add functionality to the button

--- app/src/main/java/com/example/mydiceapp/MainActivity.java
+++ app/src/main/java/com/example/mydiceapp/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.mydiceapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView diceImage1 = findViewById(R.id.imgdice1);
+        final ImageView diceImage2 = findViewById(R.id.imgdice2);
+
+        final int [] diceImages = {R.drawable.dice1, R.drawable.dice2, R.drawable.dice3, R.drawable.dice4, R.drawable.dice5, R.drawable.dice6};
+
+
+        Button myButton = (Button)findViewById(R.id.myButton);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Random rndObj = new Random();
+                int rndImage1 = rndObj.nextInt(6);
+                int rndImage2 = rndObj.nextInt(6);
+
+                diceImage1.setImageResource(diceImages[rndImage1]);
+                diceImage2.setImageResource(diceImages[rndImage2]);
+
+            }
+        });
     }
 }
