a2a948e1a07bc356999c04ab3c563cb9a0379cab
Tanesh Gupta <taneshgupta2211@gmail.com>
2020-04-21 19:09:27 +0530

    final commit

--- app/src/main/java/com/example/mydiceapp/MainActivity.java
+++ app/src/main/java/com/example/mydiceapp/MainActivity.java
@@ -21,15 +21,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ImageView diceImage1 = findViewById(R.id.imgdice1);
-        final ImageView diceImage2 = findViewById(R.id.imgdice2);
+        final ImageView diceImage1 = findViewById(R.id.imgdice1);           //creating reference of first ImageView
+        final ImageView diceImage2 = findViewById(R.id.imgdice2);           //creating reference of second ImageView
 
         final int [] diceImages = {R.drawable.dice1, R.drawable.dice2, R.drawable.dice3, R.drawable.dice4, R.drawable.dice5, R.drawable.dice6};
 
 
-        Button myButton = (Button)findViewById(R.id.myButton);
+        Button myButton = (Button)findViewById(R.id.myButton);              //creating reference of the ButtonView
 
-        final MediaPlayer mp = MediaPlayer.create(this, R.raw.dice_sound);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.dice_sound);      //for adding audio
 
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,23 +38,23 @@ public class MainActivity extends AppCompatActivity {
                 Random rndObj = new Random();
                 int rndImage = rndObj.nextInt(6);
 
-                diceImage1.setImageResource(diceImages[rndImage]);
+                diceImage1.setImageResource(diceImages[rndImage]);          //for changing first ImageView randomly
 
                 rndImage = rndObj.nextInt(6);
 
-                diceImage2.setImageResource(diceImages[rndImage]);
+                diceImage2.setImageResource(diceImages[rndImage]);          //for changing second ImageView randomly
 
-                YoYo.with(Techniques.Shake)
+                YoYo.with(Techniques.Shake)                                 //for adding shake animation to first ImageView
                         .duration(500)
                         .repeat(0)
                         .playOn(findViewById(R.id.imgdice1));
 
-                YoYo.with(Techniques.Shake)
+                YoYo.with(Techniques.Shake)                                 //for adding shake animation to second ImageView
                         .duration(500)
                         .repeat(0)
                         .playOn(findViewById(R.id.imgdice2));
 
-                mp.start();
+                mp.start();                                                 //for playing audio file
 
             }
         });
